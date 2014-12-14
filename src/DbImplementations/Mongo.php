d7eec99440a5d778af939bb9e215ff6121c1b4fb
coa <peravasa@yahoo.co.uk>
2014-12-14 18:27:35 +0100

    Work on mongo features started

--- src/DbImplementations/Mongo.php
+++ src/DbImplementations/Mongo.php
@@ -2,8 +2,53 @@
 
 namespace DbMockLibrary\DbImplementations;
 
+use DbMockLibrary\Exceptions\AlreadyInitializedException;
+use DbMockLibrary\Exceptions\DbOperationFailedException;
+use DbMockLibrary\AbstractMockLibrary;
+use SimpleArrayLibrary\SimpleArrayLibrary;
+use UnexpectedValueException;
+
 class Mongo extends AbstractMockLibrary
 {
+
+    /**
+     * @param array  $initialData
+     * @param string $serverName
+     * @param string $database
+     * @param string $username
+     * @param string $password
+     *
+     * @throws AlreadyInitializedException
+     * @return void
+     */
+    public static function init(array $initialData, $serverName, $database, $username, $password)
+    {
+        if (!self::$instance) {
+            if (empty($serverName) || !is_string($serverName)) {
+                throw new UnexpectedValueException('Invalid server name');
+            }
+            if (empty($database) || !is_string($database)) {
+                throw new UnexpectedValueException('Invalid database name');
+            }
+            if (empty($username) || !is_string($username)) {
+                throw new UnexpectedValueException('Invalid username');
+            }
+            if (!is_string($password)) {
+                throw new UnexpectedValueException('Invalid password');
+            }
+            if (!SimpleArrayLibrary::isAssociative($initialData) && !empty($initialData)) {
+                throw new UnexpectedValueException('Invalid table names');
+            }
+
+            self::$instance              = new self();
+            self::$instance->data        = self::$initialData = $initialData;
+
+            // edit $data array where needed
+            self::$instance->update();
+        } else {
+            throw new AlreadyInitializedException('MySQL library already initialized');
+        }
+    }
     /**
      * Insert into database
      *
